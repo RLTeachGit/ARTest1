f458e74a1135e5017ef7ea73f00f93061f345d22
Richard Leinfellner <richard@modulo17.com>
2018-08-01 00:23:57 +0100

    First upload

---
+++ Assets/DebugCam.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DebugCam : MonoBehaviour {
+
+    public Text DebugText;
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        DebugText.text = string.Format("{0}\n{1}", transform.position, transform.rotation.eulerAngles);
+	}
+}
---
+++ Assets/DoTrigger.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class DoTrigger : MonoBehaviour {
+
+    MeshRenderer mMR;
+    TextMeshPro mTMesh;
+
+	// Use this for initialization
+	void Start () {
+        mMR = GetComponent<MeshRenderer>();
+        mTMesh = GetComponentInChildren<TextMeshPro>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        mTMesh.text = "Hello";
+	}
+
+	private void OnTriggerEnter(Collider other)
+	{
+        mMR.material.color = Color.green;
+        mTMesh.text = "Close";
+        mTMesh.ForceMeshUpdate(true);
+	}
+
+    private void OnTriggerExit(Collider other)
+    {
+        mMR.material.color = Color.white;
+        mTMesh.text = "Far";
+        mTMesh.ForceMeshUpdate(true);
+    }
+}
